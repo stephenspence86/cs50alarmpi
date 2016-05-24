ba89d2c92b0a008e8b376509ce7bb5638a544343
Stephen Spence <stephen.spence86@gmail.com>
2016-05-24 16:42:03 +0100

    3rd cntd

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.config import Config
+from kivy.clock import Clock
+
+import time
 
 class Alarm(Widget):
     pass
 
+#class clock(Label):
+#    def update(self, *args):
+#        self.time = time.asctime()
+
 class AlarmApp(App):
     def build(self):
+        #clockLabel = clock()
+        #Clock.schedule_interval(clockLabel.update, 1)
         Window.borderless = True
-        Window.fullscreen = True
+        #Window.fullscreen = True
         return Alarm()
 
 if __name__ == '__main__':
